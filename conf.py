498f43863ce14e3116e8ea6830a4a779738c3fc5
Mike Matera <maximus@fatboycentral.com>
2018-08-27 17:36:06 -0700

    Remove tables support.

--- conf.py
+++ conf.py
@@ -40,7 +40,6 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
